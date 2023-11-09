26c503f12560201d1522459d39aaa76bfa272951
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-11-09 19:41:58 +0300

    completed task

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+import Keypad from './Keypad'
+
+//click event handler
+function handleFocus(){
+    console.log("Good!")
+}
+function handleBlur(){
+  console.log("Hey! Eyes on me!")
+}
+
+function EyesOnMe() {
+  return (
+    <div>
+        <button onBlur={handleBlur} onFocus={handleFocus}>EyesOnMe</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
-
+import React from 'react'
+function handleEvent(e){
+    console.log(`${e.target.value}`)
+}
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input
+            type="password"
+            name="password"
+            onChange={handleEvent}
+            placeholder="Enter password..."/>
+        </div>
     )
 }
 
